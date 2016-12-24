c9ded836c4c4e3c3d6a6ae7d883723a8b48b4333
emesc <tahder77@gmail.com>
2016-12-24 13:26:09 +1100

    Removed validations in item model.
    Removed validations since Faker gem is used. Files may repeat.
    Improved the controller by updating the respond_to blocks generated by
    scaffold. Removed the flash notices.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,53 +26,36 @@ class ItemsController < ApplicationController
   # POST /items.json
   def create
     @item = Item.new(item_params)
-
-    respond_to do |format|
-      if @item.save
-        format.html { redirect_to @item, notice: 'Item was successfully created.' }
-        format.json { render :show, status: :created, location: @item }
-        format.js
-      else
-        format.html { render :new }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.save
+      # flash[:notice] = "Item was successfully created."
+      respond_to :html, :js
+    else
+      render 'new'
     end
   end
 
-  # PATCH/PUT /items/1
-  # PATCH/PUT /items/1.json
   def update
     @item.remote_file_url ||= @item.remote_file_url
-    respond_to do |format|
-      if @item.update(item_params)
-        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @item }
-        format.js
-      else
-        format.html { render :edit }
-        format.json { render json: @item.errors, status: :unprocessable_entity }
-      end
+    if @item.update_attributes(item_params)
+      # flash[:notice] = "Item was successfully updated."
+      respond_to :html, :js
+    else
+      render 'edit'
     end
   end
 
-  # DELETE /items/1
-  # DELETE /items/1.json
   def destroy
     @item.destroy
-    respond_to do |format|
-      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
-      format.json { head :no_content }
-      format.js
-    end
+    # flash[:notice] = "Item successfully deleted."
+    respond_to :html, :js
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_item
       @item = Item.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
       params.require(:item).permit(:name, :description, :remote_file_url)
     end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,15 @@
 class Item < ApplicationRecord
   default_scope -> { order(id: :desc) }
 
+  # # validating this way may cause TypeError: can't cast ItemUploader
+  # # Soln: Field cannot be unique.. CarrierWave stores "no file" as empty string
   # validates_presence_of :name, :description, :file
-  validates :name, presence: true,
-                   uniqueness: { case_sensitive: false }
-  validates :description, presence: true,
-                          uniqueness: { case_sensitive: false }
-  validates :file, presence: true,
-                   uniqueness: { case_sensitive: false }
+  # validates :name, presence: true,
+  #                  uniqueness: { case_sensitive: false }
+  # validates :description, presence: true,
+  #                         uniqueness: { case_sensitive: false }
+  # validates :file, presence: true,
+  #                  uniqueness: { case_sensitive: false }
 
 
   mount_uploader :file, ItemUploader
